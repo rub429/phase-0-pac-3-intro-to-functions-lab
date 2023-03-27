0f34dc25ec21d7d091dd577dcb559f896d89b99f
Ruben <rub429@gmail.com>
2023-03-26 20:08:30 -0400

    lab

---
+++ index.js
@@ -0,0 +1,57 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    // todo
+  }
+  function whisper(string) {
+    return string;
+  }
+  "Hello!".toLowerCase(); // 'hello!'
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+function logshout(string) {
+    // todo
+  }
+  function logshout(string) {
+    console.log() string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function logshout(string) {
+    console.log() string.toUpperCase();
+  }
+function logWhisper(string) {
+    // todo
+  }
+  function logWhisper(string) {
+    console.log string;
+  }
+  "Hello!".toLowerCase(); // 'hello!'
+  function logWhisper(string) {
+    console.log string.toLowerCase();
+  }
+function sayHiToHeadphonedRoommate(string) {
+    // todo
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    return string;
+  }
+  "hello!".toLowerCase(); // 'I can't hear you!'
+  function sayHiToHeadphonedRoommate(string) {
+    return string.toLowerCase();
+  }
+  "HELLO!".toUpperCase(); // 'YES INDEED!'
+  function sayHiToHeadphonedRoommate(string) {
+    return string.toUpperCase();
+  }
+  "Let's have dinner together!".toLowerCase(); // 'I would love to!'
+  function sayHiToHeadphonedRoommate(string) {
+    return string;
